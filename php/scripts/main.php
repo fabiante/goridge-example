e97b671964c53df65a7ccabd1759546aeddbaeb7
fabiante <fabiante@users.noreply.github.com>
2023-10-02 16:10:55 +0200

    Fix redunant definitions in php code

--- php/scripts/main.php
+++ php/scripts/main.php
@@ -4,13 +4,13 @@ require_once __DIR__ . '/../vendor/autoload.php';
 
 use Spiral\Goridge;
 
-$rpc = new Goridge\RPC\RPC(
-    Goridge\Relay::create('tcp://127.0.0.1:6001')
-);
+$relay = Goridge\Relay::create('tcp://127.0.0.1:6001');
+
+$rpc = new Goridge\RPC\RPC($relay);
 
 //or, using factory:
-$tcpRPC = new Goridge\RPC\RPC(Goridge\Relay::create('tcp://127.0.0.1:6001'));
-$unixRPC = new Goridge\RPC\RPC(Goridge\Relay::create('unix:///tmp/rpc.sock'));
-$streamRPC = new Goridge\RPC\RPC(Goridge\Relay::create('pipes://stdin:stdout'));
+// $tcpRPC = new Goridge\RPC\RPC($relay);
+// $unixRPC = new Goridge\RPC\RPC(Goridge\Relay::create('unix:///tmp/rpc.sock'));
+// $streamRPC = new Goridge\RPC\RPC(Goridge\Relay::create('pipes://stdin:stdout'));
 
 echo $rpc->call("App.Hi", "Antony");
