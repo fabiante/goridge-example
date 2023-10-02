220dc87722614fa6eb34db69e7761979f9fb3ddb
fabiante <fabiante@users.noreply.github.com>
2023-10-02 17:07:38 +0200

    Add example of sending binary data (string in PHP)
    This is not yet an ideal example since it reads a whole file into memory
    instead of using some stream mechanism.

--- go/main.go
+++ go/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"net"
+	"net/http"
 	"net/rpc"
 
 	goridgeRpc "github.com/roadrunner-server/goridge/v3/pkg/rpc"
@@ -12,6 +13,13 @@ type App struct{}
 
 func (s *App) Hi(name string, r *string) error {
 	*r = fmt.Sprintf("Hello, %s!", name)
+	return nil
+}
+
+func (s *App) CountBytes(file []byte, ret *string) error {
+	mime := http.DetectContentType(file)
+
+	*ret = fmt.Sprintf("the file you sent is %d bytes long and its mime type is %s. This may or may not be accurate", len(file), mime)
 	return nil
 }
 
--- php/scripts/main.php
+++ php/scripts/main.php
@@ -2,15 +2,22 @@
 
 require_once __DIR__ . '/../vendor/autoload.php';
 
-use Spiral\Goridge;
+use Spiral\Goridge\Relay;
+use Spiral\Goridge\RPC\Codec\MsgpackCodec;
+use Spiral\Goridge\RPC\RPC;
 
-$relay = Goridge\Relay::create('tcp://127.0.0.1:6001');
+$relay = Relay::create('tcp://127.0.0.1:6001');
 
-$rpc = new Goridge\RPC\RPC($relay);
+$rpc = new RPC($relay);
+$rpc = $rpc->withCodec(new MsgpackCodec());
 
 //or, using factory:
 // $tcpRPC = new Goridge\RPC\RPC($relay);
 // $unixRPC = new Goridge\RPC\RPC(Goridge\Relay::create('unix:///tmp/rpc.sock'));
 // $streamRPC = new Goridge\RPC\RPC(Goridge\Relay::create('pipes://stdin:stdout'));
 
-echo $rpc->call("App.Hi", "Antony");
+echo $rpc->call("App.Hi", "Antony"); echo "\n";
+
+$file = file_get_contents(__DIR__ . '/main.php');
+
+echo $rpc->call("App.CountBytes", $file); echo "\n";
